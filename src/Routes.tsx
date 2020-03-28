bf7c6537992418c1dda0df72a016495c9d27b232
Gabe Pressman <gpressman@kbra.com>
2020-03-28 13:26:58 -0400

    create scaffold for People View

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Home } from './ui/Home';
+import { Home } from "./ui/Home";
+import { PeopleContainer } from "./ui/People/PeopleContainer";
 
 export const Routes = () => (
   <Router>
     <Switch>
-      <Route path="/" render={() => <Home />}/>
+      <Route path="/" render={() => <Home />} />
+      <Route path="/people" render={() => <PeopleContainer />} />}
     </Switch>
   </Router>
 );
