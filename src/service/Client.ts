4bd0786876bcfe3df208efa6ae3f515eb7522ac7
Gabe Pressman <gpressman@kbra.com>
2020-03-28 18:32:59 -0400

    add filtring of people

--- src/service/Client.ts
+++ src/service/Client.ts
@@ -1,13 +1,13 @@
-import { AxiosInstance, default as axios } from "axios";
+import { AxiosInstance, default as axios, AxiosResponse } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
   baseURL: "http://192.168.123.245/v1"
 });
 
-class Client {
-  async getPeople(params: string) {
-    return httpClient.get(`/people?${params}`);
+class TraceClient {
+  async getPeople(params: string): Promise<AxiosResponse> {
+    return httpClient.get(`/people${params}`);
   }
 }
 
-export const client = new Client();
+export const traceClient = new TraceClient();
