6d2d2d450eee78d7dee0230d1d9d9802ec810286
Hunter Jennings <jenningsdhunter@gmail.com>
2020-03-28 15:04:28 -0400

    Adding react router dom types

--- src/ui/People/People.tsx
+++ src/ui/People/People.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { Person } from "./PeopleContainer";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import { Person } from './PeopleContainer';
+import { Link } from 'react-router-dom';
 
 export const People = ({ people }: PeopleProps) => {
   return (
